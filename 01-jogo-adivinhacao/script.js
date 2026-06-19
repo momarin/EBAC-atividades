f6bfa84ded0c55d13912ec8dbc99fac917cc1c18
Marina de Moraes <mrndemoraes@gmail.com>
2026-06-19 16:39:46 -0300

    alteracoes no html e inicio js

---
+++ 01-jogo-adivinhacao/script.js
@@ -0,0 +1,13 @@
+// -------------- PUXANDO ELEMENTOS DO DOM
+const btnIniciar = document.querySelector(".btn-iniciar");
+const btnChutar = document.querySelector(".btn-chutar");
+const inputNumero = document.querySelector(".input-numero");
+const mensagem = document.querySelector("mensagem");
+const tentativas = document.querySelector(".tentativas");
+const instrucoes = document.querySelector(".instrucoes");
+const areaJogo = document.querySelector(".area-jogo");
+
+// -------------- EVENTOS
+btnIniciar.addEventListener("click", function () {
+  console.log("ok");
+});
