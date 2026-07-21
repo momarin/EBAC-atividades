e3382bdf3cf2c8bea9532687a2274fe1fd527c4e
Marina de Moraes <mrndemoraes@gmail.com>
2026-07-21 18:12:42 -0300

    cadastro funcional

--- 04-cadastro-cliente-crudcrud/script.js
+++ 04-cadastro-cliente-crudcrud/script.js
@@ -6,6 +6,27 @@ const campoNome = document.getElementById("nome");
 const campoEmail = document.getElementById("email");
 const btnCadastro = document.querySelector(".btn-primary");
 const btnLimpar = document.querySelector(".btn-secondary");
+const listaClientes = document.querySelector("ul");
 /* ===================== FUNÇÕES ===================== */
+// Cadastrar
+btnCadastro.addEventListener("click", async (event) => {
+  event.preventDefault();
 
-/* ===================== EVENT LISTENERS ===================== */
+  const nome = campoNome.value.trim();
+  const email = campoEmail.value.trim();
+
+  if (!nome || !email) {
+    alert("Preencha todos os campos.");
+    return;
+  }
+
+  await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ nome, email }),
+  });
+
+  campoNome.value = "";
+  campoEmail.value = "";
+  campoNome.focus();
+});
