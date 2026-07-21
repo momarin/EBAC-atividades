d7ea4979a8109648110527ace936a63cb5d10f2f
Marina de Moraes <mrndemoraes@gmail.com>
2026-07-21 18:27:58 -0300

    get post funcional

--- 04-cadastro-cliente-crudcrud/script.js
+++ 04-cadastro-cliente-crudcrud/script.js
@@ -8,9 +8,32 @@ const btnCadastro = document.querySelector(".btn-primary");
 const btnLimpar = document.querySelector(".btn-secondary");
 const listaClientes = document.querySelector("ul");
 /* ===================== FUNÇÕES ===================== */
+// Listar Clientes Cadastrados
+async function listarClientes() {
+  const resposta = await fetch(API_URL);
+  const clientes = await resposta.json();
+
+  listaClientes.innerHTML = "";
+
+  if (clientes.length === 0) {
+    listaClientes.innerHTML =
+      '<li class="empty">Nenhum cliente cadastrado</li>';
+    return;
+  }
+  clientes.forEach((cliente) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <div>
+                <strong>${cliente.nome}</strong> - ${cliente.email}
+                <button class="btn-delete" data-id="${cliente.id}">X</button>
+            </div>
+  `;
+    listaClientes.appendChild(li);
+  });
+}
 // Cadastrar
-btnCadastro.addEventListener("click", async (event) => {
-  event.preventDefault();
+btnCadastro.addEventListener("click", async (e) => {
+  e.preventDefault();
 
   const nome = campoNome.value.trim();
   const email = campoEmail.value.trim();
@@ -29,4 +52,15 @@ btnCadastro.addEventListener("click", async (event) => {
   campoNome.value = "";
   campoEmail.value = "";
   campoNome.focus();
+
+  listarClientes();
 });
+// Limpar Campos
+btnLimpar.addEventListener("click", (e) => {
+  e.preventDefault();
+  campoNome.value = "";
+  campoEmail.value = "";
+  campoNome.focus();
+});
+
+listarClientes();
