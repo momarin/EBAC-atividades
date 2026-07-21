47b698fda0028b8290a48130384e1ac118fdd55b
Marina de Moraes <mrndemoraes@gmail.com>
2026-07-21 18:05:34 -0300

    criação dos esqueletos + json

---
+++ 04-cadastro-cliente-crudcrud/script.js
@@ -0,0 +1,11 @@
+// ===================== URL DA API =====================
+const API_URL = "http://localhost:3000/clientes";
+
+/* ===================== DOM ===================== */
+const campoNome = document.getElementById("nome");
+const campoEmail = document.getElementById("email");
+const btnCadastro = document.querySelector(".btn-primary");
+const btnLimpar = document.querySelector(".btn-secondary");
+/* ===================== FUNÇÕES ===================== */
+
+/* ===================== EVENT LISTENERS ===================== */
